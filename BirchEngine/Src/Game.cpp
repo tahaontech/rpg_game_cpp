4e514e99643ad01010ff4553398d3716deea72f3
tahatech <thhk2831@gmail.com>
2023-12-02 13:05:47 +0330

    publish

---
+++ BirchEngine/Src/Game.cpp
@@ -0,0 +1,152 @@
+#include "Game.h"
+#include "TextureManager.h"
+#include "Map.h"
+#include "ECS/Components.h"
+#include "ECS/ColliderComponent.h"
+#include "Vector2D.h"
+#include "Collision.h"
+
+Map* map;
+Manager manager;
+
+SDL_Renderer* Game::renderer = nullptr;
+SDL_Event Game::event;
+SDL_Rect Game::camera = { 0, 0, 800, 640 };
+
+std::vector<ColliderComponent*> Game::colliders;
+bool Game::isRunning = false;
+
+auto& player(manager.addEntity());
+
+
+
+
+Game::Game()
+{}
+
+Game::~Game()
+{}
+
+void Game::init(const char* title, int width, int height, bool fullscreen)
+{
+	int flags = 0;
+	
+	if (fullscreen)
+	{
+		flags = SDL_WINDOW_FULLSCREEN;
+	}
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
+		window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
+		renderer = SDL_CreateRenderer(window, -1, 0);
+		if (renderer)
+		{
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		}
+
+		isRunning = true;
+	}
+
+	map = new Map("assets/terrain_ss.png", 3, 32);
+
+	// ecs implementation
+
+	map->LoadMap("assets/map.map", 25, 20);
+
+	player.addComponent<TransformComponent>(3);
+	player.addComponent<SpriteComponent>("assets/player_anims.png", true);
+	player.addComponent<KeyBoardController>();
+	player.addComponent<ColliderComponent>("player");
+	player.addGroup(groupPlayers);
+
+}
+
+auto& tiles(manager.getGroup(Game::groupMap));
+auto& players(manager.getGroup(Game::groupPlayers));
+auto& tcolliders(manager.getGroup(Game::groupColliders));
+
+void Game::handleEvents()
+{
+
+	SDL_PollEvent(&event);
+
+	switch (event.type)
+	{
+	case SDL_QUIT :
+		isRunning = false;
+		break;
+	default:
+		break;
+	}
+}
+
+void Game::update()
+{
+	SDL_Rect playerCol = player.getComponent<ColliderComponent>().collider;
+	Vector2D playerPos = player.getComponent<TransformComponent>().position;
+
+	manager.refresh();
+	manager.update();
+
+	for (auto& c : tcolliders)
+	{
+		SDL_Rect cCol = c->getComponent<ColliderComponent>().collider;
+		if (Collision::AABB(cCol, playerCol))
+		{
+			player.getComponent<TransformComponent>().position = playerPos;
+		}
+	}
+
+	camera.x = player.getComponent<TransformComponent>().position.x - 400;
+	camera.y = player.getComponent<TransformComponent>().position.y - 320;
+
+	if (camera.x < 0)
+	{
+		camera.x = 0;
+	}
+	if (camera.y < 0)
+	{
+		camera.y = 0;
+	}
+	if (camera.x > camera.w)
+	{
+		camera.x = camera.w;
+	}
+	if (camera.y > camera.h)
+	{
+		camera.y = camera.h;
+	}
+}
+
+
+
+void Game::render()
+{
+	SDL_RenderClear(renderer);
+	
+	for (auto& t : tiles)
+	{
+		t->draw();
+	}
+	for (auto& c : tcolliders)
+	{
+		c->draw();
+	}
+	for (auto& p : players)
+	{
+		p->draw();
+	}
+	
+
+	SDL_RenderPresent(renderer);
+}
+
+void Game::clean()
+{
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+}
+
+
---
+++ BirchEngine/Src/Map.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <string>
+
+class Map {
+
+public:
+
+	Map(const char* mfp, int ms, int ts);
+	~Map();
+
+	void LoadMap(std::string path, int sizeX, int sizeY);
+	void AddTile(int srcX, int srcY, int xpos, int ypos);
+private:
+	const char* mapFilePath;
+	int mapScale;
+	int tileSize;
+	int scaledsize;
+};
